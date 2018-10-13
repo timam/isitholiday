6b118878821cd6db3f5ef6a9c47ab27c0295ef05
timam <me@timam.io>
2018-10-13 19:29:47 +0600

    initial model

--- checker/models.py
+++ checker/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+Sunday = 'Sunday'
+Monday = 'Monday'
+Tuesday = 'Tuesday'
+Wednesday = 'Wednesday'
+Thursday = 'Thursday'
+Friday = 'Friday'
+Saturday = 'Saturday'
+
+DAYS_OF_WEEK = (
+    (Sunday, 'Sunday'),
+    (Monday, 'Monday'),
+    (Tuesday, 'Tuesday'),
+    (Wednesday, 'Wednesday'),
+    (Thursday, 'Thursday'),
+    (Friday, 'Friday'),
+    (Saturday, 'Saturday'),
+)
+
+Holiday = 'Holiday'
+NotHoliday = 'NotHoliday'
+
+STATUS = (
+    (Holiday, 'Holiday'),
+    (NotHoliday, 'NotHoliday'),
+)
+
+
+class Entry(models.Model):
+    day = models.IntegerField(primary_key=True)
+    date = models.DateField(null=False, blank=False)
+    weekday = models.CharField(max_length=10, choices=DAYS_OF_WEEK)
+    status = models.CharField(max_length=10, choices=STATUS)
+    description = models.TextField()
+
+    def __str__(self):
+        return str(self.day)
