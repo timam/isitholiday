93d13c1109d54cba08380014232a30ccceb9c4c2
timam <me@timam.io>
2018-10-16 18:07:48 +0600

    templates & static added

--- checker/views.py
+++ checker/views.py
@@ -1,15 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Entry
 import datetime
 
 # Create your views here.
 
-gimme_date = datetime.datetime.now().date()
-entry = Entry.objects.filter(date__exact=gimme_date).get()
-status = entry.status
-
 
 def index(request):
 
-    return HttpResponse(status)
+    gimme_date = datetime.datetime.now().date()
+    entry = Entry.objects.filter(date__exact=gimme_date).get()
+    status = entry.status
+    context = {"status": status}
 
+    return render(request, 'index.html', context)
